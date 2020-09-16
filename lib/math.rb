a52868e9260261b05ea018797fc8d50d14fc5a77
Michael Lee <michael.l33.1994@gmail.com>
2020-09-16 19:51:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,12 +5,12 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  difference = num1 + num2
+  difference = num1 - num2
   difference
 end
 
 def division(num1, num2)
-  quotient = num1 + num2
+  quotient = num1 / num2
   quotient
 end
 
